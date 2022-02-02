4eb1fc58e3754377b05cdf74d1d9310714712cec
ihunter <ijh.hunter@gmail.com>
2022-02-02 02:06:32 -0500

    firebase analytics

--- src/db.js
+++ src/db.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/messaging'
+import 'firebase/analytics'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBV9G06TKa-B6LF7fe63Z7QFbm8PJU7ad4',
@@ -9,7 +10,8 @@ const firebaseConfig = {
   projectId: 'jerma-logs',
   storageBucket: 'jerma-logs.appspot.com',
   messagingSenderId: '123570718331',
-  appId: '1:123570718331:web:f246438a6c717efd10c4de'
+  appId: '1:123570718331:web:f246438a6c717efd10c4de',
+  measurementId: 'G-R7QL1P3TGG'
 }
 
 // Get a Firestore instance
