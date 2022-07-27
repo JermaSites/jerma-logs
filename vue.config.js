54b52ef0a9032d8008046230e4f14f497a2ef85c
ihunter <ijh.hunter@gmail.com>
2022-07-26 22:40:29 -0400

    update packages

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,34 @@
-module.exports = {
+const { defineConfig } = require("@vue/cli-service");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false,
+  configureWebpack: {
+    plugins: [new NodePolyfillPlugin()],
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+      },
+    },
+  },
   pwa: {
-    name: 'Jerma Logs',
-    themeColor: '#0afc9e',
-    msTileColor: '#0afc9e',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-    workboxPluginMode: 'InjectManifest',
+    name: "Jerma Logs",
+    themeColor: "#0afc9e",
+    msTileColor: "#0afc9e",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    workboxPluginMode: "InjectManifest",
     workboxOptions: {
-      swSrc: 'src/firebase-messaging-sw.js'
-    }
+      swSrc: "src/firebase-messaging-sw.js",
+    },
   },
   css: {
     loaderOptions: {
       sass: {
         additionalData: `
           @import "@/assets/scss/_variables.scss";
-        `
-      }
-    }
-  }
-}
+        `,
+      },
+    },
+  },
+});
