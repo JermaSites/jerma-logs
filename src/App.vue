592843e4ef9b3f7454653cc43ca26fe21dd7d053
ihunter <ijh.hunter@gmail.com>
2022-11-16 19:22:14 -0500

    discord link

--- src/App.vue
+++ src/App.vue
@@ -8,5 +8,14 @@ import TheHeader from "./components/TheHeader.vue";
     <main class="container mx-auto px-2 md:max-w-6xl">
       <router-view />
     </main>
+    <section>
+      <div class="p-6 text-center">
+        Message me on Discord for any issues (<a
+          href="https://discordapp.com/users/Modus%20Pwnens#9109"
+          class="text-blue-400 hover:underline"
+          >Modus Pwnens#9109</a
+        >)
+      </div>
+    </section>
   </div>
 </template>
