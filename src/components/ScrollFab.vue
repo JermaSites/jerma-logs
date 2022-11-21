1f9bb024830211d210062404259a6404e37fdbb1
ihunter <ijh.hunter@gmail.com>
2022-11-21 15:39:44 -0500

    only show fab on scroll

--- src/components/ScrollFab.vue
+++ src/components/ScrollFab.vue
@@ -1,5 +1,25 @@
 <script setup>
+import { useWindowScroll } from "@vueuse/core";
 import { ChevronUpIcon, ChevronDownIcon } from "@heroicons/vue/24/solid";
+import { ref, watch } from "vue";
+
+const { y } = useWindowScroll();
+
+const showFab = ref(false);
+const showFabTimeout = ref(null);
+
+watch(y, () => {
+  showFab.value = true;
+
+  if (showFabTimeout.value) {
+    clearTimeout(showFabTimeout.value);
+  }
+
+  showFabTimeout.value = setTimeout(() => {
+    console.log("setTimeout");
+    showFab.value = false;
+  }, 3000);
+});
 
 function scrollToTop() {
   window.scroll({
@@ -19,21 +39,32 @@ function scrollToBottom() {
 </script>
 
 <template>
-  <div class="fixed z-90 bottom-6 right-6 flex flex-col gap-5">
-    <button
-      @click="scrollToTop"
-      class="bg-slate-800 w-14 h-14 rounded-full flex justify-center items-center border-2 border-slate-500 drop-shadow-md"
-    >
-      <ChevronUpIcon class="w-6 text-blue-500 bg-slate-800" />
-    </button>
-
-    <button
-      @click="scrollToBottom"
-      class="bg-slate-800 w-14 h-14 rounded-full flex justify-center items-center border-2 border-slate-500 drop-shadow-md"
-    >
-      <ChevronDownIcon class="w-6 text-blue-500 bg-slate-800" />
-    </button>
-  </div>
+  <Transition>
+    <div v-if="showFab" class="fixed z-90 bottom-6 right-6 flex flex-col gap-5">
+      <button
+        @click="scrollToTop"
+        class="bg-slate-800 w-14 h-14 rounded-full flex justify-center items-center border-2 border-slate-500 drop-shadow-md"
+      >
+        <ChevronUpIcon class="w-6 text-blue-500 bg-slate-800" />
+      </button>
+      <button
+        @click="scrollToBottom"
+        class="bg-slate-800 w-14 h-14 rounded-full flex justify-center items-center border-2 border-slate-500 drop-shadow-md"
+      >
+        <ChevronDownIcon class="w-6 text-blue-500 bg-slate-800" />
+      </button>
+    </div>
+  </Transition>
 </template>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
