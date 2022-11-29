9291474b4350fdf5b602ae18a1a5b5da64ea1b9f
ihunter <ijh.hunter@gmail.com>
2022-11-29 02:08:57 -0500

    add header link to home add width and height to logo img

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,9 +4,12 @@ import logoUrl from "../assets/images/logo.png";
 
 <template>
   <header class="bg-slate-900">
-    <div class="container flex mx-auto items-center p-4 md:max-w-6xl">
-      <img :src="logoUrl" alt="Jerma Logs Logo" />
-      <h1 class="ml-6 text-slate-200 text-5xl text-center">Jerma Logs</h1>
+    <div class="container mx-auto p-4 md:max-w-6xl">
+      <router-link :to="{ name: 'Home' }" class="flex items-center">
+        <img :src="logoUrl" width="128" height="128" alt="Jerma Logs Logo" />
+
+        <h1 class="text-slate-200 text-5xl ml-6">Jerma Logs</h1>
+      </router-link>
     </div>
   </header>
 </template>
