2282128a6cc0165ac16c16c02f76d2fd1d4e88d9
ihunter <ijh.hunter@gmail.com>
2023-11-13 18:06:53 -0500

    test fix for FCM on firefox

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,9 +1,30 @@
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
-importScripts(
-  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
-);
+// importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+// importScripts(
+//   "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
+// );
 
-firebase.initializeApp({
+// firebase.initializeApp({
+//   apiKey: "AIzaSyBV9G06TKa-B6LF7fe63Z7QFbm8PJU7ad4",
+//   authDomain: "jerma-logs.firebaseapp.com",
+//   databaseURL: "https://jerma-logs.firebaseio.com",
+//   projectId: "jerma-logs",
+//   storageBucket: "jerma-logs.appspot.com",
+//   messagingSenderId: "123570718331",
+//   appId: "1:123570718331:web:f246438a6c717efd10c4de",
+//   measurementId: "G-R7QL1P3TGG",
+// });
+
+// const messaging = firebase.messaging();
+
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging/sw";
+import { onBackgroundMessage } from "firebase/messaging/sw";
+
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+const firebaseApp = initializeApp({
   apiKey: "AIzaSyBV9G06TKa-B6LF7fe63Z7QFbm8PJU7ad4",
   authDomain: "jerma-logs.firebaseapp.com",
   databaseURL: "https://jerma-logs.firebaseio.com",
@@ -14,4 +35,19 @@ firebase.initializeApp({
   measurementId: "G-R7QL1P3TGG",
 });
 
-const messaging = firebase.messaging();
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = getMessaging(firebaseApp);
+
+onBackgroundMessage(messaging, (payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
