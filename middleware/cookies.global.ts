d4d3d2e82fefe5c7546553e347ed3f701220394a
ihunter <ijh.hunter@gmail.com>
2024-01-09 03:56:32 -0400

    remove dayjs imports from middleware

--- middleware/cookies.global.ts
+++ middleware/cookies.global.ts
@@ -1,10 +1,3 @@
-import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc.js";
-import timezone from "dayjs/plugin/timezone";
-
-dayjs.extend(utc);
-dayjs.extend(timezone);
-
 export default defineNuxtRouteMiddleware((to, from) => {
   // Change meaningless variable to update cookie expiration date
   const sortStore = useSortStore();
