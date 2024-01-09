0447cbd4010f0be12b03e913d838911c2fa4de85
ihunter <ijh.hunter@gmail.com>
2024-01-09 03:52:03 -0500

    move dynamic color hue for dark/light mode into util

--- components/Message.vue
+++ components/Message.vue
@@ -6,6 +6,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import advancedFormat from "dayjs/plugin/advancedFormat";
 
 import type { Message } from "@/types";
+import dynamicHue from "~/utils/dynamicHue";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -35,41 +36,7 @@ const messageSentAtTimeAgo = computed(() => {
 const colorMode = useColorMode();
 const messageColor = computed(() => {
   const hex = props.message.color;
-
-  let r = parseInt("0x" + hex[1] + hex[2]);
-  let g = parseInt("0x" + hex[3] + hex[4]);
-  let b = parseInt("0x" + hex[5] + hex[6]);
-
-  r /= 255;
-  g /= 255;
-  b /= 255;
-
-  let cmin = Math.min(r, g, b),
-    cmax = Math.max(r, g, b),
-    delta = cmax - cmin,
-    h = 0,
-    s = 0,
-    l = 0;
-
-  if (delta == 0) h = 0;
-  else if (cmax == r) h = ((g - b) / delta) % 6;
-  else if (cmax == g) h = (b - r) / delta + 2;
-  else h = (r - g) / delta + 4;
-
-  h = Math.round(h * 60);
-
-  if (h < 0) h += 360;
-
-  l = (cmax + cmin) / 2;
-  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-  s = +(s * 100).toFixed(1);
-  l = +(l * 100).toFixed(1);
-
-  if (colorMode.value === "light") {
-    l -= 15;
-  }
-
-  return "hsl(" + h + "," + s + "%," + l + "%)";
+  return dynamicHue(hex, colorMode.value);
 });
 
 const { parseEmotes } = useEmotes();
