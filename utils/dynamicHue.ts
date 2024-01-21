1a4249977c2cf684964facadb818e7fd3684af0c
ihunter <ijh.hunter@gmail.com>
2024-01-21 01:48:23 -0500

    move hexToHSL into its own util function

--- utils/dynamicHue.ts
+++ utils/dynamicHue.ts
@@ -1,42 +1,11 @@
+import hexToHSL from "./hexToHSL";
+
 export default function (hex: string, colorMode: string): string {
-  let { h, l, s } = hexToHLS(hex);
+  let { h, s, l } = hexToHSL(hex);
 
   if (colorMode === "light") {
     l -= 15;
   }
 
   return "hsl(" + h + "," + s + "%," + l + "%)";
-}
-
-function hexToHLS(hex: string) {
-  let r = parseInt("0x" + hex[1] + hex[2]);
-  let g = parseInt("0x" + hex[3] + hex[4]);
-  let b = parseInt("0x" + hex[5] + hex[6]);
-
-  r /= 255;
-  g /= 255;
-  b /= 255;
-
-  let cmin = Math.min(r, g, b),
-    cmax = Math.max(r, g, b),
-    delta = cmax - cmin,
-    h = 0,
-    s = 0,
-    l = 0;
-
-  if (delta == 0) h = 0;
-  else if (cmax == r) h = ((g - b) / delta) % 6;
-  else if (cmax == g) h = (b - r) / delta + 2;
-  else h = (r - g) / delta + 4;
-
-  h = Math.round(h * 60);
-
-  if (h < 0) h += 360;
-
-  l = (cmax + cmin) / 2;
-  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-  s = +(s * 100).toFixed(1);
-  l = +(l * 100).toFixed(1);
-
-  return { h, l, s };
 }
