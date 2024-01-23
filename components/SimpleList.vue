76f5527ee265fbc6fde0e3ace460cdff162fe559
ihunter <ijh.hunter@gmail.com>
2024-01-23 18:13:18 -0500

    remove comments

--- components/SimpleList.vue
+++ components/SimpleList.vue
@@ -1,21 +1,7 @@
-<script setup lang="ts">
-// type Props = {
-//   items: Array<any>;
-//   itemKey: string;
-// };
-
-// defineProps<Props>();
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <ul>
     <slot />
-    <!-- <li
-      v-for="item in items"
-      :key="item[itemKey]"
-      class="odd:bg-slate-300 even:bg-slate-200 dark:odd:bg-slate-900 dark:even:bg-slate-800"
-    >
-      <slot :item="item" />
-    </li> -->
   </ul>
 </template>
