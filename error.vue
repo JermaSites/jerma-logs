9133b7f5fdf775807dd51854f22db45c609ac2a3
ihunter <ijh.hunter@gmail.com>
2024-08-06 19:03:17 -0400

    404 page

---
+++ error.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const error = useError()
+
+function handleError() {
+  clearError({
+    redirect: '/',
+  })
+}
+</script>
+
+<template>
+  <NuxtLayout name="error">
+    <div class="text-center">
+      <h1 class="text-9xl">
+        {{ error.statusCode }}
+      </h1>
+      <h2 class="text-6xl">
+        {{ error.message }}
+      </h2>
+
+      <button class=" border-slate-200 bg-slate-800 hover:bg-slate-900 font-bold tracking-wide p-4 mt-4" @click="handleError">
+        Return to Home
+      </button>
+    </div>
+  </NuxtLayout>
+</template>
+
+<style scoped>
+
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+useHead({
+  bodyAttrs: {
+    class:
+      'min-h-screen bg-slate-50 text-slate-950 dark:bg-slate-700 dark:text-slate-200',
+  },
+})
+</script>
+
+<template>
+  <NuxtLoadingIndicator />
+
+  <SiteHeader class="mb-4" />
+
+  <main class="container mx-auto px-4 md:max-w-6xl">
+    <slot />
+  </main>
+  <SiteFooter />
+</template>
+
+<style>
+:root {
+  font-family: "Roboto", sans-serif;
+}
+</style>
