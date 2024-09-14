ec5b6e61543a2c669995bf6798161032b01316cf
ihunter <ijh.hunter@gmail.com>
2024-09-14 13:34:39 -0400

    disbale all logic on messages

--- pages/[year]/[month]/index.vue
+++ pages/[year]/[month]/index.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 import type { RouteLocationNormalizedLoaded } from '#vue-router'
 import type { Breadcrumb, Message } from '@/types'
-import {
-  collection,
-  onSnapshot,
-  query,
-  type Unsubscribe,
-  where,
-} from 'firebase/firestore'
+// import {
+//   collection,
+//   onSnapshot,
+//   query,
+//   type Unsubscribe,
+//   where,
+// } from 'firebase/firestore'
 
-const dayjs = useDayjs()
+// const dayjs = useDayjs()
 const { capitalize } = useCapitalize()
 const route = useRoute()
 
@@ -52,21 +52,21 @@ definePageMeta({
   },
 })
 
-const { fetchEmotes, parseEmotes } = useEmotes()
-const { fetchBadges, parseBadges } = useBadges()
+// const { fetchEmotes, parseEmotes } = useEmotes()
+// const { fetchBadges, parseBadges } = useBadges()
 
-fetchEmotes()
-fetchBadges()
+// fetchEmotes()
+// fetchBadges()
 
 const { year, month } = route.params as { year: string, month: string }
 
-const isCurrentMonth = computed(() => {
-  const currentDate = dayjs.utc()
-  const date = dayjs.utc(`${year}-${capitalize(month)}-01`, 'YYYY-MMMM-DD')
-  const endTime = date.endOf('month')
+// const isCurrentMonth = computed(() => {
+//   const currentDate = dayjs.utc()
+//   const date = dayjs.utc(`${year}-${capitalize(month)}-01`, 'YYYY-MMMM-DD')
+//   const endTime = date.endOf('month')
 
-  return endTime.isAfter(currentDate)
-})
+//   return endTime.isAfter(currentDate)
+// })
 
 const { data: messages, status } = await useFetch<Message[]>(
   `/api/messages/${year}/${month}`,
@@ -75,57 +75,57 @@ const { data: messages, status } = await useFetch<Message[]>(
   },
 )
 
-const { twitchUsername } = useRuntimeConfig().public
-const { db } = useFirebase()
-const unsub = ref<Unsubscribe>()
+// const { twitchUsername } = useRuntimeConfig().public
+// const { db } = useFirebase()
+// const unsub = ref<Unsubscribe>()
 
-onMounted(async () => {
-  const date = dayjs.utc(`${year}-${capitalize(month)}-01`, 'YYYY-MMMM-DD')
-  const startTime = date.startOf('month').valueOf().toString()
-  const endTime = date.endOf('month').valueOf().toString()
+// onMounted(async () => {
+//   const date = dayjs.utc(`${year}-${capitalize(month)}-01`, 'YYYY-MMMM-DD')
+//   const startTime = date.startOf('month').valueOf().toString()
+//   const endTime = date.endOf('month').valueOf().toString()
 
-  if (!isCurrentMonth.value)
-    return
+//   if (!isCurrentMonth.value)
+//     return
 
-  const q = query(
-    collection(db, 'messages'),
-    where('sentAt', '>=', startTime),
-    where('sentAt', '<=', endTime),
-    where('username', '==', twitchUsername),
-  )
+//   const q = query(
+//     collection(db, 'messages'),
+//     where('sentAt', '>=', startTime),
+//     where('sentAt', '<=', endTime),
+//     where('username', '==', twitchUsername),
+//   )
 
-  unsub.value = onSnapshot(q, (querySnapshot) => {
-    if (querySnapshot.docs.length === 0)
-      return
-    messages.value = querySnapshot.docs.map(doc => doc.data() as Message)
-  })
-})
+//   unsub.value = onSnapshot(q, (querySnapshot) => {
+//     if (querySnapshot.docs.length === 0)
+//       return
+//     messages.value = querySnapshot.docs.map(doc => doc.data() as Message)
+//   })
+// })
 
-onUnmounted(() => {
-  if (!unsub.value)
-    return
-  unsub.value()
-})
+// onUnmounted(() => {
+//   if (!unsub.value)
+//     return
+//   unsub.value()
+// })
 
-const sortStore = useSortStore()
-const { sortOrder } = storeToRefs(sortStore)
+// const sortStore = useSortStore()
+// const { sortOrder } = storeToRefs(sortStore)
 
-const sortedMessages = computed(() => {
-  return messages.value
-  // return messages?.value?.toSorted((a, b) => {
-  //   if (sortOrder.value.message === 'asc')
-  //     return Number.parseInt(a.sentAt) - Number.parseInt(b.sentAt)
+// const sortedMessages = computed(() => {
+//   return messages.value
+//   // return messages?.value?.toSorted((a, b) => {
+//   //   if (sortOrder.value.message === 'asc')
+//   //     return Number.parseInt(a.sentAt) - Number.parseInt(b.sentAt)
 
-  //   return Number.parseInt(b.sentAt) - Number.parseInt(a.sentAt)
-  // })
-})
+//   //   return Number.parseInt(b.sentAt) - Number.parseInt(a.sentAt)
+//   // })
+// })
 </script>
 
 <template>
   <section>
-    <div v-if="sortedMessages && sortedMessages.length !== 0">
+    <div v-if="messages && messages.length !== 0">
       <SimpleList>
-        <SimpleListItem v-for="message in sortedMessages" :key="message.id">
+        <SimpleListItem v-for="message in messages" :key="message.id">
           <Message
             :sent-at="message.sentAt"
             :display-name="message.displayName"
@@ -141,7 +141,7 @@ const sortedMessages = computed(() => {
       <SimpleListSkeleton :rows="15" />
     </div>
 
-    <div v-else-if="sortedMessages && sortedMessages.length === 0" class="p-8 text-center text-5xl md:text-8xl">
+    <div v-else-if="messages && messages.length === 0" class="p-8 text-center text-5xl md:text-8xl">
       <h1>No Messages Found</h1>
     </div>
   </section>
