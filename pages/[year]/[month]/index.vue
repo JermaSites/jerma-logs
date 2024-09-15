d6e819c00060ac5c21e1c4c97b6d9b4a68918d78
ihunter <ijh.hunter@gmail.com>
2024-09-15 04:24:58 -0400

    no logic, just display messages

--- pages/[year]/[month]/index.vue
+++ pages/[year]/[month]/index.vue
@@ -53,90 +53,93 @@ definePageMeta({
 
 const { year, month } = route.params as { year: string, month: string }
 
-const { data: messages, status } = await useFetch<Message[]>(
+const { data: messages } = await useFetch<Message[]>(
   `/api/messages/${year}/${month}`,
   {
-    lazy: true,
+    lazy: false,
     server: true,
   },
 )
 
-const { fetchEmotes, parseEmotes } = useEmotes()
-const { fetchBadges, parseBadges } = useBadges()
+// const { fetchEmotes, parseEmotes } = useEmotes()
+// const { fetchBadges, parseBadges } = useBadges()
 
-fetchEmotes()
-fetchBadges()
+// fetchEmotes()
+// fetchBadges()
 
-const dayjs = useDayjs()
+// const dayjs = useDayjs()
 
-function isCurrentMonth() {
-  const currentDate = dayjs.utc()
-  const date = dayjs.utc(`${year}-${capitalize(month)}-01`, 'YYYY-MMMM-DD')
-  const endTime = date.endOf('month')
+// function isCurrentMonth() {
+//   const currentDate = dayjs.utc()
+//   const date = dayjs.utc(`${year}-${capitalize(month)}-01`, 'YYYY-MMMM-DD')
+//   const endTime = date.endOf('month')
 
-  return endTime.isAfter(currentDate)
-}
+//   return endTime.isAfter(currentDate)
+// }
 
-const { twitchUsername } = useRuntimeConfig().public
-const { db } = useFirebase()
-const unsub = ref<Unsubscribe>()
+// const { twitchUsername } = useRuntimeConfig().public
+// const { db } = useFirebase()
+// const unsub = ref<Unsubscribe>()
 
-onMounted(async () => {
-  const date = dayjs.utc(`${year}-${capitalize(month)}-01`, 'YYYY-MMMM-DD')
-  const startTime = date.startOf('month').valueOf().toString()
-  const endTime = date.endOf('month').valueOf().toString()
+// onMounted(async () => {
+//   const date = dayjs.utc(`${year}-${capitalize(month)}-01`, 'YYYY-MMMM-DD')
+//   const startTime = date.startOf('month').valueOf().toString()
+//   const endTime = date.endOf('month').valueOf().toString()
 
-  if (!isCurrentMonth())
-    return
+//   if (!isCurrentMonth())
+//     return
 
-  const q = query(
-    collection(db, 'messages'),
-    where('sentAt', '>=', startTime),
-    where('sentAt', '<=', endTime),
-    where('username', '==', twitchUsername),
-  )
+//   const q = query(
+//     collection(db, 'messages'),
+//     where('sentAt', '>=', startTime),
+//     where('sentAt', '<=', endTime),
+//     where('username', '==', twitchUsername),
+//   )
 
-  unsub.value = onSnapshot(q, (querySnapshot) => {
-    if (querySnapshot.docs.length === 0)
-      return
-    messages.value = querySnapshot.docs.map(doc => doc.data() as Message)
-  })
-})
+//   unsub.value = onSnapshot(q, (querySnapshot) => {
+//     if (querySnapshot.docs.length === 0)
+//       return
+//     messages.value = querySnapshot.docs.map(doc => doc.data() as Message)
+//   })
+// })
 
-onUnmounted(() => {
-  if (!unsub.value)
-    return
-  unsub.value()
-})
+// onUnmounted(() => {
+//   if (!unsub.value)
+//     return
+//   unsub.value()
+// })
 
-const sortStore = useSortStore()
-const { sortOrder } = storeToRefs(sortStore)
+// const sortStore = useSortStore()
+// const { sortOrder } = storeToRefs(sortStore)
 
-const sortedMessages = computed(() => {
-  return messages?.value?.toSorted((a, b) => {
-    if (sortOrder.value.message === 'asc')
-      return Number.parseInt(a.sentAt) - Number.parseInt(b.sentAt)
+// const sortedMessages = computed(() => {
+//   return messages?.value?.toSorted((a, b) => {
+//     if (sortOrder.value.message === 'asc')
+//       return Number.parseInt(a.sentAt) - Number.parseInt(b.sentAt)
 
-    return Number.parseInt(b.sentAt) - Number.parseInt(a.sentAt)
-  })
-})
+//     return Number.parseInt(b.sentAt) - Number.parseInt(a.sentAt)
+//   })
+// })
 
-const messagesFound = computed(() => {
-  return (Boolean(messages.value) && messages.value?.length !== 0)
-})
+// const messagesFound = computed(() => {
+//   return (Boolean(messages.value) && messages.value?.length !== 0)
+// })
 
-const isLoading = computed(() => status.value === 'pending' || status.value === 'idle')
+// const isLoading = computed(() => status.value === 'pending' || status.value === 'idle')
 </script>
 
 <template>
   <section>
-    <div v-if="isLoading">
+    <div v-for="{ id, message } in messages" :key="id">
+      {{ message }}
+    </div>
+
+    <!-- <div v-if="isLoading">
       <SimpleListSkeleton :rows="15" />
     </div>
 
     <div v-else-if="messagesFound">
-      {{ sortedMessages }}
-      <!-- <SimpleList>
+      <SimpleList>
         <SimpleListItem v-for="message in sortedMessages" :key="message.id">
           <Message
             :sent-at="message.sentAt"
@@ -146,12 +149,12 @@ const isLoading = computed(() => status.value === 'pending' || status.value === 
             :badges="parseBadges(message.badges)"
           />
         </SimpleListItem>
-      </SimpleList> -->
+      </SimpleList>
     </div>
 
     <div v-else class="p-8 text-center text-5xl md:text-8xl">
       <h1>No Messages Found</h1>
-    </div>
+    </div> -->
   </section>
 </template>
 
