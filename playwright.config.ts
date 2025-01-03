69dbb03662669a5369faa217cac11a5af74e9267
ihunter <ijh.hunter@gmail.com>
2025-01-03 13:56:43 -0500

    fix webkit playwright scale

--- playwright.config.ts
+++ playwright.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use: { ...devices['Desktop Safari'], deviceScaleFactor: 1 },
     },
 
     /* Test against mobile viewports. */
