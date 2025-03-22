ff42a58ea5d39320a88897a9e1ab1be8b251f1d6
ihunter <ijh.hunter@gmail.com>
2025-03-21 23:21:05 -0400

    update USkeleton to @nuxt/ui v3

--- components/SimpleListSkeleton.vue
+++ components/SimpleListSkeleton.vue
@@ -8,9 +8,9 @@ defineProps<{
   <SimpleList>
     <SimpleListItem v-for="i in $props.rows" :key="i" class="h-14 p-5">
       <div class="flex gap-1">
-        <USkeleton class="h-4 w-[200px]" :ui="{ background: 'bg-slate-400 dark:bg-slate-600' }" />
-        <USkeleton class="h-4 w-[150px]" :ui="{ background: 'bg-slate-400 dark:bg-slate-600' }" />
-        <USkeleton class="h-4 w-full" :ui="{ background: 'bg-slate-400 dark:bg-slate-600' }" />
+        <USkeleton class="h-4 w-[200px] bg-slate-400 dark:bg-slate-600" />
+        <USkeleton class="h-4 w-[150px] bg-slate-400 dark:bg-slate-600" />
+        <USkeleton class="h-4 w-full bg-slate-400 dark:bg-slate-600" />
       </div>
     </SimpleListItem>
   </SimpleList>
