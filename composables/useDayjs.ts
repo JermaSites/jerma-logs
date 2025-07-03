2fdbcb43421d716f0e363a2e533afcbad365ad44
ihunter <ijh.hunter@gmail.com>
2025-07-02 23:58:52 -0400

    fix dayjs plugin imports

--- composables/useDayjs.ts
+++ composables/useDayjs.ts
@@ -1,8 +1,8 @@
 import dayjs from 'dayjs'
-import advancedFormat from 'dayjs/plugin/advancedFormat'
-import relativeTime from 'dayjs/plugin/relativeTime'
-import timezone from 'dayjs/plugin/timezone'
-import utc from 'dayjs/plugin/utc'
+import advancedFormat from 'dayjs/plugin/advancedFormat.js'
+import relativeTime from 'dayjs/plugin/relativeTime.js'
+import timezone from 'dayjs/plugin/timezone.js'
+import utc from 'dayjs/plugin/utc.js'
 
 dayjs.extend(advancedFormat)
 dayjs.extend(relativeTime)
