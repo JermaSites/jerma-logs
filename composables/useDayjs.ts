aaa5ee4f4468f4205a594d79d38927b87309344b
ihunter <ijh.hunter@gmail.com>
2025-07-02 23:35:45 -0400

    remove dayjs plugin in favor of composable

--- composables/useDayjs.ts
+++ composables/useDayjs.ts
@@ -1,4 +1,13 @@
 import dayjs from 'dayjs'
+import advancedFormat from 'dayjs/plugin/advancedFormat'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import timezone from 'dayjs/plugin/timezone'
+import utc from 'dayjs/plugin/utc'
+
+dayjs.extend(advancedFormat)
+dayjs.extend(relativeTime)
+dayjs.extend(timezone)
+dayjs.extend(utc)
 
 export default function () {
   return dayjs
