0fdc9ab5538aa4fc90667354ebb134b4856d5cf2
ihunter <ijh.hunter@gmail.com>
2026-07-31 00:09:57 -0400

    Refactor SimpleListSkeleton component for consistent skeleton widths

--- app/components/SimpleListSkeleton.vue
+++ app/components/SimpleListSkeleton.vue
@@ -8,8 +8,8 @@ defineProps<{
   <SimpleList>
     <SimpleListItem v-for="i in rows" :key="i" class="h-14 p-5">
       <div class="flex gap-1">
-        <USkeleton class="h-4 w-[200px] bg-slate-400 dark:bg-slate-600" />
-        <USkeleton class="h-4 w-[150px] bg-slate-400 dark:bg-slate-600" />
+        <USkeleton class="h-4 w-50 bg-slate-400 dark:bg-slate-600" />
+        <USkeleton class="h-4 w-37.5 bg-slate-400 dark:bg-slate-600" />
         <USkeleton class="h-4 w-full bg-slate-400 dark:bg-slate-600" />
       </div>
     </SimpleListItem>
